37e7752d88a935a9647391cb4fac1acceb74c1e2
Tim Garvin <tcgarvin@gmail.com>
2015-04-27 07:28:31 -0400

    Now bluemix compatible.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "tcgarvin.com - Hello" });
 });
 
 module.exports = router;
