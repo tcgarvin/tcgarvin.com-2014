3143bd4220f417c7f17d76fe76b02a391620c74b
Tim Garvin <tcgarvin@gmail.com>
2017-06-11 20:06:21 -0400

    Added a project showcase

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: "Hello - tcgarvin.com" });
+  res.render('index', { title: "Hello - tcgarvin.com", homepage: true });
 });
 
 router.get('/blackjack', function(req, res) {
   res.render('blackjack', { title: "Blackjack Simulation - tcgarvin.com" });
+});
+
+router.get('/code', function(req, res) {
+  res.render('code', { title: "Tim's Code - tcgarvin.com" });
 });
 
 module.exports = router;
